3458f64f1e0457c3ec5e61b759310b30a3dae0cf
MaxiSanc37 <maxijsanchez37@gmail.com>
2025-02-10 17:31:49 +0000

    player movement and default arena - the arena and platform have not got colliders yet

--- Assets/Scripts/CharacterMov.cs
+++ Assets/Scripts/CharacterMov.cs
@@ -1,18 +1,89 @@
+using System.Buffers.Text;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static TMPro.SpriteAssetUtilities.TexturePacker_JsonArray;
 
 public class CharacterMov : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public float m_Speed = 12f;
+    public float m_TurnSpeed = 180f;
+
+    private string m_MovementAxisName;
+    private string m_TurnAxisName;
+    private Rigidbody m_Rigidbody;
+    private float m_MovementInputValue;
+    private float m_TurnInputValue;
+
+    private void Awake()
     {
-        
+        m_Rigidbody = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
-    void Update()
+
+    private void OnEnable()
     {
-        
+        m_Rigidbody.isKinematic = false;
+        m_MovementInputValue = 0f;
+        m_TurnInputValue = 0f;
+    }
+
+
+    private void OnDisable()
+    {
+        m_Rigidbody.isKinematic = true;
+    }
+
+
+    private void Start()
+    {
+        m_MovementAxisName = "Vertical";
+        m_TurnAxisName = "Horizontal";
+    }
+
+    private void Update()
+    {
+        // Store the player's input and make sure the audio for the engine is playing.
+        m_MovementInputValue = Input.GetAxis(m_MovementAxisName);
+        m_TurnInputValue = Input.GetAxis(m_TurnAxisName);
+
+    }
+
+    private void FixedUpdate()
+    {
+        // Move and turn the player.
+        // Adjust the rigidbodies position and orientation in FixedUpdate. 
+        Move();
+        Turn();
+    }
+
+
+    private void Move()
+    {
+        // Adjust the position of the player based on the player's input.
+
+        // Create a vector in the direction the player is facing with a magnitude 
+        //based on the input, speed and the time between frames.
+        Vector3 movement = transform.forward * m_MovementInputValue * m_Speed *
+        Time.deltaTime;
+
+        // Apply this movement to the rigidbody's position. 
+        m_Rigidbody.MovePosition(m_Rigidbody.position + movement);
+    }
+
+
+    private void Turn()
+    {
+        // Adjust the rotation of the player based on the player's input.
+
+        // Determine the number of degrees to be turned based on the input,   
+        //speed and time between frames.
+        float turn = m_TurnInputValue * m_TurnSpeed * Time.deltaTime;
+
+        // Make this into a rotation in the y axis. 
+        Quaternion turnRotation = Quaternion.Euler(0f, turn, 0f);
+
+        // Apply this rotation to the rigidbody's rotation. 
+        m_Rigidbody.MoveRotation(m_Rigidbody.rotation * turnRotation);
     }
 }
